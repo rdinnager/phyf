31075676899dc8559665961ba373c76235a7627e
rdinnager <r.dinnage@gmail.com>
2022-10-28 14:35:42 -0400

    started adding mammal biogeography data to phyf

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,10 @@
 ## create AVONET dataset
 library(phyf)
 library(dplyr)
+library(sf)
+library(ape)
+library(fasterize)
+library(raster)
 
 avonet_tree <- ape::read.nexus("extdata/HackettStage1_0001_1000_MCCTreeTargetHeights.nex")
 avonet_dat <- readr::read_csv("extdata/AVONET3_BirdTree.csv")
@@ -23,3 +27,49 @@ vert_bmr <- vert_bmr %>%
               rename(label = `...1`))
 
 usethis::use_data(vert_bmr, overwrite = TRUE)
+
+
+#### Mammal Biogeography
+
+mammal_tree <- ape::read.nexus("extdata/terrestrial_mammal_tree_matching_IUCN.nexus")
+mammal_maps <- sf::read_sf("extdata/MAMMALS")
+ecoregions <- sf::st_read("extdata/Ecoregions")
+
+ecoregions <- ecoregions %>% 
+  dplyr::select(ECO_NAME, ECO_ID) %>%
+  st_make_valid()
+
+ecoregions_rast <- fasterize(ecoregions, raster::raster(ecoregions, resolution = 0.1),
+                             field = "ECO_ID")
+
+mammal_maps <- mammal_maps %>%
+  dplyr::select(binomial, presence, origin, seasonal) %>%
+  filter(origin %in% c(1, 2)) %>%
+  filter(presence %in% c(1, 4, 5)) %>%
+  dplyr::select(binomial) %>%
+  st_make_valid()
+
+not_valid <- !st_is_valid(mammal_maps)
+sum(not_valid)
+plot(mammal_maps[not_valid, "geometry"])
+
+mammal_maps <- mammal_maps[!not_valid, ]
+
+mammal_maps <- mammal_maps %>%
+  group_by(binomial) %>%
+  summarise()
+
+nrow(mammal_maps)
+
+readr::write_rds(mammal_maps, "exdata/mammal_ranges_processed.rds")
+
+ecoregion_ex <- raster::extract(ecoregions_rast, mammal_maps)
+
+mammal_biogeo <- pf_as_pf(mammal_tree)
+mammal_biogeo <- mammal_biogeo %>%
+  left_join(vert_dat %>%
+              rename(label = `...1`))
+
+usethis::use_data(vert_bmr, overwrite = TRUE)
+
+
