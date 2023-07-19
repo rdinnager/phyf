e30e6318b3e0a1a6ab23b0e9f7d0f9e308f17a93
rdinnager <r.dinnage@gmail.com>
2023-07-19 10:39:01 -0400

    added `pf_is_empty()` function which tests for empty elements in a pfc.

--- R/phyf-vctrs.R
+++ R/phyf-vctrs.R
@@ -1244,4 +1244,20 @@ drop_root_edges <- function(x, ...) {
   pf_as_pfc(m, internal = internal[-1:-mrca],
             is_tip = field(x, "is_tip"))
 }
+
+#' Test which elements of a pfc are empty
+#'
+#' @param x A `pfc` object
+#' @param ... Further arguments for future extensions. Currently not used.
+#'
+#' @return A logical vector of the same length as `x` with `TRUE` where 
+#' there are empty flows in `x`, `FALSE` otherwise
+#' @export
+#'
+#' @examples
+#' pf_is_empty(rpfc(100))
+pf_is_empty <- function(x, ...) {
+  purrr::map_lgl(pf_path(x),
+                 purrr::is_empty)
+} 
   
